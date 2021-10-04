96c98f1032bee999b6fb9591f3cbf575abdf777f
Dovran <dowran@Dowrans-MBP.lan>
2021-10-04 18:09:06 -0400

    Added delete todoList and checkItems

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
-const Todo = ({ text }) => {
-  console.log(text)
+const Todo = ({ text, todo, todos, setTodos }) => {
+  // console.log(text)
+
+  const deleteHandler = () => {
+    // console.log(todo)
+    setTodos(todos.filter((el) => el.id !== todo.id))
+  }
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          }
+        }
+        return item
+      })
+    )
+  }
+
   return (
     <div>
-      <li>{text}</li>
-      <button type='check'>Check</button>
-      <button>Delete</button>
+      <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+        {text}
+      </li>
+      <button type='check' onClick={completeHandler}>
+        Check
+      </button>
+      <button onClick={deleteHandler}>Delete</button>
     </div>
   )
 }
